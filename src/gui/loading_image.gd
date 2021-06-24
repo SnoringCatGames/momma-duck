1ecde620462a550b30e88706b93e4f5db13f5b5d
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-06-24 14:15:15 -0700

    Fix accordion caret padding

--- src/gui/loading_image.gd
+++ src/gui/loading_image.gd
@@ -13,7 +13,6 @@ var _is_ready := false
 func _ready() -> void:
     _is_ready = true
     
-    set_meta("gs_rect_position", rect_position)
     set_meta("gs_rect_size", rect_size)
     set_meta("gs_rect_min_size", rect_min_size)
     
@@ -26,12 +25,10 @@ func set_base_scale(value: float) -> void:
 
 
 func update_gui_scale() -> bool:
-    var original_rect_position: Vector2 = get_meta("gs_rect_position")
     var original_rect_size: Vector2 = get_meta("gs_rect_size")
     var original_rect_min_size: Vector2 = get_meta("gs_rect_min_size")
 
     var scale: float = _base_scale * Gs.gui.scale
-    rect_position = original_rect_position * scale
     rect_min_size = original_rect_min_size * scale
     rect_size = original_rect_size * scale
     $Control.rect_position = LOADING_IMAGE_SIZE * 0.5 * scale
