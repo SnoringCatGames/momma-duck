c2f2ddb1ae63ede4db9d1181d8f2ff353ad53b90
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-07-13 14:24:58 -0700

    Refactor bootstrap/app-config system to require only a single invocation for client

--- src/config/main.gd
+++ src/config/main.gd
@@ -3,22 +3,7 @@ extends SurfacerBootstrap
 
 
 func _ready() -> void:
-    run(MommaDuck.app_manifest, self)
-
-
-func _amend_app_manifest() -> void:
-    MommaDuck.amend_app_manifest(MommaDuck.app_manifest)
-    ._amend_app_manifest()
-
-
-#func _register_app_manifest() -> void:
-#    ._register_app_manifest()
-
-
-func _initialize_framework() -> void:
-    ._initialize_framework()
-    MommaDuck.initialize()
-    MommaDuck.load_state()
+    run(MommaDuck, self)
 
 
 func _on_app_initialized() -> void:
@@ -28,11 +13,3 @@ func _on_app_initialized() -> void:
     Surfacer.annotators.set_annotator_enabled(
             AnnotatorType.RECENT_MOVEMENT,
             false)
-
-
-#func _on_splash_finished() -> void:
-#    ._on_splash_finished()
-
-
-#func _on_app_quit() -> void:
-#    ._on_app_quit()
