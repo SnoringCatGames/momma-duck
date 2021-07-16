83e6279b49a8f95895cff923ecfa3e3e6e09c140
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-07-16 08:54:53 -0700

    Update READMEs, update screen accept/exit input handling to use registered actions.

--- src/config/main.gd
+++ src/config/main.gd
@@ -1,17 +1,2 @@
 class_name Main
 extends Node
-
-
-func _ready() -> void:
-    Sc.connect("initialized", self, "_on_scaffolder_initialized")
-    Sc.connect("splashed", self, "_on_splash_screen_finished")
-
-
-func _on_scaffolder_initialized() -> void:
-    Sc.disconnect("initialized", self, "_on_scaffolder_initialized")
-    pass
-
-
-func _on_splash_screen_finished() -> void:
-    Sc.disconnect("splashed", self, "_on_splash_screen_finished")
-    pass
