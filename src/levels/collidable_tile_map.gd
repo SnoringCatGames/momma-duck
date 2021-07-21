eb2b12c089b0e21e049e3d57968f9ccfaa1efa99
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-07-21 13:08:40 -0700

    Add a handy export property for drawing tilemap indices, to help with debugging.

--- src/levels/collidable_tile_map.gd
+++ src/levels/collidable_tile_map.gd
@@ -7,6 +7,23 @@ const DEFAULT_TILE_SET := \
         preload("res://src/levels/collidable_tile_set.tres")
 
 
+export var draws_tile_indices := false setget _set_draws_tile_indices
+
+
 func _ready() -> void:
     if tile_set == null:
         tile_set = DEFAULT_TILE_SET
+
+
+func _draw() -> void:
+    if draws_tile_indices:
+        Sc.draw.draw_tile_map_indices(
+                    self,
+                    self,
+                    Color.white,
+                    false)
+
+
+func _set_draws_tile_indices(value: bool) -> void:
+    draws_tile_indices = value
+    update()
