af995f9d6163bbd46164681966ef58a4da3017a2
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-08-06 12:04:07 -0700

    Fix the how the Spider animator is configured with its non-standard sprite names.

--- src/players/spider/spider_animator.gd
+++ src/players/spider/spider_animator.gd
@@ -6,28 +6,12 @@ extends ScaffolderPlayerAnimator
 var is_facing_down := true
 
 
-func animation_name_to_sprite(animation_name: String) -> Sprite:
-    match animation_name:
-        "ClimbUp":
-            return $ClimbUp as Sprite
-        "ClimbDown":
-            return $ClimbDown as Sprite
-        "Rest":
-            if is_facing_down:
-                return $RestDown as Sprite
-            else:
-                return $RestUp as Sprite
-        _:
-            Sc.logger.error()
-            return null
-
-
-func animation_name_to_playback_rate(animation_name: String) -> float:
-    match animation_name:
-        "Rest":
-            if is_facing_down:
-                return animations["RestDown"].speed
-            else:
-                return animations["RestUp"].speed
-        _:
-            return animations[animation_name].speed
+func _standand_animation_name_to_specific_animation_name(
+        standard_name: String) -> String:
+    if standard_name == "Rest":
+        if is_facing_down:
+            return "RestDown"
+        else:
+            return "RestUp"
+    else:
+        return standard_name
